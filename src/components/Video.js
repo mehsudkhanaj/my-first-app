429fb8cb5dfd4bf1e9df07c108afae79e8d0ea82
jamshed <jamshed2019@namal.edu.pk>
2024-01-01 13:53:48 +0500

    React Internship

---
+++ src/components/Video.js
@@ -0,0 +1,23 @@
+function Video(){
+    return (
+        <>
+    <div>
+        VIdeo 
+        <Thumb/>
+        
+    </div>
+        </>
+    )
+
+}
+function Thumb(){
+    return (
+        
+    <div>
+        Thumb
+    </div>
+        
+    )
+
+}
+export {Video, Thumb};
